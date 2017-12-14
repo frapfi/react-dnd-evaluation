afc32ee35bb6554022a0a3c3a2c26c6dcc8c7b8e
frapfi <frapfist@gmail.com>
2017-12-14 09:13:38 +0100

    refactored to compositional components

--- src/components/DraggableZone.js
+++ src/components/DraggableZone.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
-import LittleBlackBox from './LittleBlackBox';
-import LittleGreyBox from './LittleGreyBox';
 
 export default class DraggableZone extends Component {
-
 	render() {
+		const dz_style = {
+			height: '200px',
+			border: '5px solid',
+			width: '35%',
+
+
+		};
+
 		return (
-			<div className="col-sm-4" style={{
-				height: '200px',
-				border: '5px solid',
-				width: '35%'
-			}}>
+			<div className="col-sm-4" style={dz_style}>
 				<div className="row">
-					<LittleBlackBox/>
-					<LittleGreyBox/>
+					{this.props.children}
 				</div>
-
 			</div>
-		)
+		);
 	}
-
 }
--- src/components/app.js
+++ src/components/app.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
 import DropBox from './DropBox';
 import DraggableZone from './DraggableZone';
-import LittleBlackBox from './LittleBlackBox';
-import LittleGreyBox from './LittleGreyBox';
 
+import LittleDragBox from './LittleDragBox';
 
 export default class App extends Component {
-  render() {
-    return (
-      <div className="row">
-		  <DropBox/>
-		  <DraggableZone/>
-	  </div>
-    );
-  }
+	render() {
+		return (
+			<div className="row">
+				<DropBox />
+				<DraggableZone>
+					<LittleDragBox color="black" />
+					<LittleDragBox color="grey" />
+					<LittleDragBox color="orange" />
+					<LittleDragBox color="blue" />
+				</DraggableZone>
+			</div>
+		);
+	}
 }
-
-//toDo
-/*
-<DraggableZone>
-	<BlackBox color="black">
-	<GreyBox color= "white">
-<DraggableZone/>
-*/
